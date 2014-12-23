9b1c0d34581cf023be027e53897bea1297f2cf72
thinkscientist <ashminhas@Ashs-MBP.home>
2014-12-23 17:30:02 +0000

    Added all the assets

---
+++ 99RedBalloons/Balloon.swift
@@ -0,0 +1,17 @@
+//
+//  Balloon.swift
+//  99RedBalloons
+//
+//  Created by Ash Minhas on 14/11/2014.
+//  Copyright (c) 2014 Ash Minhas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon {
+    var number = 0
+    var image = UIImage(named: "")
+    
+}
+
